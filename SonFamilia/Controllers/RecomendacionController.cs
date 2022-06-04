934b2a42ab3f828731439950422eefa84832b615
Neiser Diaz <N00022213@upn.pe>
2022-06-03 21:33:19 -0500

    vista recomendacion

---
+++ SonFamilia/Controllers/RecomendacionController.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SonFamilia.Database;
+using SonFamilia.Models;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SonFamilia.Controllers
+{
+    public class RecomendacionController : Controller
+    {
+        private readonly SfContext context;
+        public RecomendacionController(SfContext context)
+        {
+            this.context = context;
+        }
+      
+        public IActionResult Index()
+        {
+            Usuario user = LoggedUser();
+            if (user != null)
+            {
+                ViewBag.Usuario = user;
+            }
+           
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private Usuario LoggedUser()
+        {
+            var claim = HttpContext.User.Claims.FirstOrDefault();
+            if (claim != null)
+            {
+                var user = context.Usuarios.Where(o => o.Correo == claim.Value).FirstOrDefault();
+                return user;
+            }
+            return null;
+        }
+    }
+}
